63ee2f6be49a5c39119081e49ffb6fb8b085c6b1
marianareis <hello@marireis.com>
2014-04-05 20:17:32 +0100

    First attempt at Rock paper scissors js

---
+++ week_4/starter_code 2/rock_paper_scissors/js/main.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+var botChoice = Math.random(),
+    humanChoiceRock = document.getElementById("rock"),
+    humanChoicePaper = document.getElementById("paper"),
+    humanChoiceScissors = document.getElementById("scissors"),
+    humanScore = document.getElementById("humanScore"),
+    computerscore = document.getElementById("computerScore");
+
+
+
+
+
+
+
+
+
