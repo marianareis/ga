403dd73b5fa0ce784e16d283e208f7563ba1d611
marianareis <hello@marireis.com>
2014-04-05 22:19:29 +0100

    my idea of how the game could work

--- week_4/starter_code 2/rock_paper_scissors/js/main.js
+++ week_4/starter_code 2/rock_paper_scissors/js/main.js
@@ -1,16 +1,80 @@
 window.onload = function() {
-var botChoice = Math.random(),
-    humanChoiceRock = document.getElementById("rock"),
-    humanChoicePaper = document.getElementById("paper"),
-    humanChoiceScissors = document.getElementById("scissors"),
-    humanScore = document.getElementById("humanScore"),
-    computerscore = document.getElementById("computerScore");
+  var humanChoice, computerChoice,
+      $humanScore, $computerScore, $buttons, $entryStatus;
 
+  $buttons = document.querySelectorAll("button");
+  $humanScore = document.getElementById("humanScore");
+  $computerScore = document.getElementById("computerScore");
+  $entryStatus = document.getElementById("status");
 
+  for (var i = 0; i < $buttons.length; i++) {
+    $buttons[i].addEventListener("click", function(event) {
+      humanChoice = event.target.attributes.id.value;
+      computerChoice = computerPick();
 
+      if (humanChoice === computerChoice) {
+        updateScores("draw");
+      }
+      else if (humanChoice === "rock") {
+        if (computerChoice === "paper") {
+          updateScores("computer");
+        }
+        else if (computerChoice === "scissors") {
+          updateScores("human");
+        }
+      }
+      else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+          updateScores("human");
+        }
+        else if (computerChoice === "scissors") {
+          updateScores("computer");
+        }
+      }
+      else { // scissors
+        if (computerChoice === "paper") {
+          updateScores("human");
+        }
+        else if (computerChoice === "rock") {
+          updateScores("computer");
+        }
+      }
+    });
+  };
 
+  var computerPick = function computerPick() {
+    var randomNumber = Math.random();
 
+    if (randomNumber < 0.34) {
+      return "rock";
+    }
+    else if (randomNumber < 0.67) {
+      return "paper";
+    }
+    else {
+      return "scissors";
+    }
+  }
 
+  var updateScores = function updateScores(winner) {
+    var score, string;
 
+    if (winner === "computer") {
+      score = parseInt($computerScore.innerHTML) + 1;
+      $computerScore.innerHTML = score;
 
+      string = "<p>You chose <u>" + humanChoice + "</u>. The bot chose <u>" + computerChoice + "</u>. You lose.</p>";
+    }
+    else if (winner === "human") {
+      score = parseInt($humanScore.innerHTML) + 1;
+      $humanScore.innerHTML = score;
 
+      string = "<p>You chose <u>" + humanChoice + "</u>. The bot chose <u>" + computerChoice + "</u>. You win.</p>";
+    }
+    else {
+      string = "<p>You chose <u>" + humanChoice + "</u>. The bot chose <u>" + computerChoice + "</u>. It's a tie.</p>";
+    }
+
+    $entryStatus.innerHTML = string;
+  }
+}
